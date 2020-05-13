2db1e99bb4c1d65e4fd97a1c4bf7281952d0941e
vysery98 <luispsi98@gmail.com>
2020-05-12 21:17:28 -0500

    Correccion Algoritmo

--- AnalAlg/src/BinarySearch/MainSearch.java
+++ AnalAlg/src/BinarySearch/MainSearch.java
@@ -8,13 +8,23 @@ public class MainSearch {
 
         // Scanner para lectura de Datos
         Scanner scan = new Scanner(System.in);
+
+        // Declaración de datos
         int values[], tamanio;
+
+        // Ingreso de datos
         System.out.println("Tamaño del arreglo: ");
         tamanio = scan.nextInt();
+
+        // Inicialización de array
         values = new int[tamanio];
+
+        // Generación de datos aleatorios para array
         for (int i = 0; i < values.length; i++) {
             values[i] = (int) (Math.random() * 100);
         }
+
+        // Proceso de búsqueda binaria
         for (int i = 0; i < 10000000; i++) {
             int index = -1;
             for (int j = 0; j < values.length; j++) {
@@ -27,5 +37,6 @@ public class MainSearch {
                 }
             }
         }
+
     }
 }
